b5d6087d964559bb6e822a8a3085df9eea32f745
martinbraas <martin.hatting@hotmail.com>
2021-11-25 15:46:06 +0100

    Progess on send mail

--- src/ui/compose.py
+++ src/ui/compose.py
@@ -1,6 +1,14 @@
 from PySide2.QtWidgets import QHBoxLayout, QPushButton, QTextEdit, QVBoxLayout, QWidget, QLineEdit, QFormLayout, QSpacerItem
 
 from backend import variables
+from backend import server as sv
+from backend import mail as em
+
+# v = variables
+recipient_email = "a"
+recipient_name = "a"
+mail_subject = "a"
+mail_body = "a"
 
 class ComposePage(QWidget):
     def __init__(self, parent = None):
@@ -27,4 +35,20 @@ class ComposePage(QWidget):
         btn_layout.addWidget(self.attachment_btn)
         btn_layout.addWidget(self.send_btn)
         btn_layout.insertStretch(1, 1)
+
+    # def send_mail():
+    #     server = sv.Server(v.smtp_serv, 0, v.port_w_tls, v.port)
+    #     server.connect()
+
+    #     email = em.Email()
+    #     with open('../../messages.txt', 'r') as f:
+    #         message = f.read()
+    #     email.setBody(message)
+
+    #     email.setRecipient(recipient_name, recipient_email)
+    #     email.setSubject(mail_subject)
+    #     server.login(v.email_adress, v.email_password)
+    #     server.send(v.email_adress, recipient_email, email.getString())
+
+    #     server.quit()
 
